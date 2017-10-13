55bf5e9d13831c0f435dcf1135038b15d6c395e4
ZDEAM <1241577948@qq.com>
2017-10-13 15:15:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/fragmentdemo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.fragmentdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.testlayout.R;
+
+/**
+ * 程序的主界面。
+ * 
+ * @author guolin
+ */
+public class MainActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
---
+++ app/src/main/java/com/example/fragmentdemo/SoundActivity.java
@@ -0,0 +1,21 @@
+package com.example.fragmentdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.testlayout.R;
+
+/**
+ * 声音界面的Activity，加入了sound_activity的布局。
+ * 
+ * @author guolin
+ */
+public class SoundActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.sound_activity);
+	}
+
+}
